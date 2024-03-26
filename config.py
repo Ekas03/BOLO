67aa74459404de7deda942f0c6b94916295f60f5
Egor Skorodumov <edskorodumov_1@edu.hse.ru>
2024-03-26 23:02:45 +0300

    feat: db settings

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-BOT_TOKEN = '6880106682:AAEuon0FRgkdyfkVCwcimoStSRmwRr2ckLU'
+# Bot token
+TOKEN = '6880106682:AAEuon0FRgkdyfkVCwcimoStSRmwRr2ckLU'
+
+# Настройки SSH
+SSH_HOST = '65.109.0.67'
+SSH_PORT = 22
+SSH_USER = 'root'
+SSH_PASSWORD = ''
+
+# Настройки базы данных
+DB_USER = 'root'
+DB_PASSWORD = 'egor123sql'
+DB_HOST = 'localhost'
+DB_PORT = 3306
+DB_NAME = 'bolo_bolo'
