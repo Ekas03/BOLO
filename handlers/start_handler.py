2fcff69c2fbf771c78f2f5f7d99da28918e3fe9f
abobrayen <abobrayen_1@edu.hse.ru>
2024-03-29 14:45:07 +0300

    feat: updated main and start_handler

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,10 +1,11 @@
-from aiogram.filters import CommandStart
+import uuid
+from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from main import router
 from database.database import SessionLocal
-from database.crud import get_user_by_telegram_id
+from database.crud import get_user_by_telegram_id, check_invitation_exists_by_uuid, handle_invitation_code
 
 # Класс регистрации
 class Registration(StatesGroup):
@@ -22,3 +23,23 @@ async def start(message: Message, state: FSMContext = None):
                 await state.set_state(Registration.waiting_for_name)
             else:
                 await message.answer(f"Привет, {get_user_by_telegram_id(db, message.from_user.id)}!")
+
+# Обработка кода приглашения
+@router.message(Command("add"))
+async def process_invitation_code(message: Message):
+    _, invitation_code = message.text.split(maxsplit=1)
+    try:
+        uuid.UUID(invitation_code)
+    except ValueError:
+        await message.answer("Введите код приглашения через пробел.\nПример: /add 12345678")
+        return
+    with SessionLocal() as db:
+        if check_invitation_exists_by_uuid(db, invitation_code):
+            new_couple = handle_invitation_code(db, invitation_code, message.from_user.id)
+            if new_couple:
+                await message.answer("Успешное подключение! Теперь вы можете погрузиться в мир BOLO!")
+                await start(message=message)
+            else:
+                await message.answer("Возникла ошибка. Пожалуйста, попробуйте еще раз.")
+        else:
+            await message.answer("Возникла ошибка. Пожалуйста, попробуйте еще раз.")
