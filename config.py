029b4bcd785bb329e58e55b01533e50b452aab92
Egor Skorodumov <edskorodumov_1@edu.hse.ru>
2024-03-31 02:00:19 +0300

    feat: FTP

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ SSH_PORT = 22
 SSH_USER = 'root'
 SSH_PASSWORD = ''
 
+# Настройки FTP - протокол для передачи файлов
+FTP_HOST = '51.210.63.139'
+FTP_USER = 'bolo'
+FTP_PASS = 'aA2pL8aN1c'
+FTP_DIR = '/www/bolobot.xyz/maps/'
+FTP_PIC_DIR = '/www/bolobot.xyz/src/'
+
+PIC_ADDR = 'https://bolobot.xyz/src/'
+
 # Настройки базы данных
 DB_USER = 'root'
 DB_PASSWORD = 'egor123sql'
