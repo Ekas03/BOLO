3a34b492448d38ad029f73f03170c7d3b12dd6fc
Egor <69798266+Ekas03@users.noreply.github.com>
2024-04-19 21:29:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,17 @@
 TOKEN = ''
 
 # Настройки FTP - протокол для передачи файлов
-FTP_HOST = '51.210.63.139'
-FTP_USER = 'bolo'
-FTP_PASS = 'aA2pL8aN1c'
-FTP_DIR = '/www/bolobot.xyz/maps/'
-FTP_PIC_DIR = '/www/bolobot.xyz/src/'
+FTP_HOST = ''
+FTP_USER = ''
+FTP_PASS = ''
+FTP_DIR = ''
+FTP_PIC_DIR = ''
 
-PIC_ADDR = 'https://bolobot.xyz/src/'
+PIC_ADDR = ''
 
 # Настройки базы данных
 DB_USER = 'root'
-DB_PASSWORD = 'egor123sql'
+DB_PASSWORD = ''
 DB_HOST = 'localhost'
 DB_PORT = 3306
-DB_NAME = 'bolo_bolo'
+DB_NAME = ''
